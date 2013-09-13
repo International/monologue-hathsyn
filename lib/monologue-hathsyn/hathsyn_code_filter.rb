a409739eb42b11a1a1c8f13f2bfb5f979cf279ab
George Opritescu <ssscripting@gmail.com>
2013-09-14 01:55:45 +0300

    Rails.logger calls

--- lib/monologue-hathsyn/hathsyn_code_filter.rb
+++ lib/monologue-hathsyn/hathsyn_code_filter.rb
@@ -2,8 +2,11 @@ module MonologueHathsyn
   class HathsynCodeFilter < HTML::Pipeline::Filter
     def call
       doc.search("[data-lang]").each do |code_node|
+        Rails.logger.info "Found node #{code_node.text}"
         actual_lang = code_node["data-lang"]
         text = code_node.text
+        replacement = CodeRay.scan(text, actual_lang).div
+        Rails.logger.info "Replacing with #{replacement}"
         code_node.replace(CodeRay.scan(text, actual_lang).div)
       end
 
