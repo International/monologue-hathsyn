686077fad8357abc9ef0469a6c7e157b55dd0402
George Opritescu <ssscripting@gmail.com>
2013-09-14 03:42:48 +0300

    Use nokogiri directly

--- app/models/monologue/posts_revision_decorator.rb
+++ app/models/monologue/posts_revision_decorator.rb
@@ -1,8 +1,13 @@
 Monologue::PostsRevision.class_eval do
   def content
     db_content = read_attribute(:content)
-    pipeline = HTML::Pipeline.new([MonologueHathsyn::HathsynCodeFilter])
-    result = pipeline.call(db_content)
-    result[:output].to_s
+    doc = Nokogiri::HTML.fragment(db_content)
+    doc.search("[data-lang]").each do |code_node|
+      actual_lang = code_node["data-lang"]
+      text = code_node.text
+      replacement = CodeRay.scan(text, actual_lang).div
+      code_node.replace(CodeRay.scan(text, actual_lang).div)
+    end
+    doc.to_html
   end
 end
