35040271893bcec7574d67a06ef6eb1477f8e770
George Opritescu <ssscripting@gmail.com>
2013-09-15 23:46:00 +0300

    More output

--- app/models/monologue/posts_revision_decorator.rb
+++ app/models/monologue/posts_revision_decorator.rb
@@ -2,12 +2,15 @@ Monologue::PostsRevision.class_eval do
   def content
     db_content = read_attribute(:content)
     doc = Nokogiri::HTML.fragment(db_content)
+    puts "Reading fragment :|#{db_content}|"
     doc.search("[data-lang]").each do |code_node|
       actual_lang = code_node["data-lang"]
       text = code_node.text
       replacement = CodeRay.scan(text, actual_lang).div
       code_node.replace(CodeRay.scan(text, actual_lang).div)
     end
-    doc.to_html
+    res = doc.to_html
+    puts "Result:|#{res}|"
+    res
   end
 end
