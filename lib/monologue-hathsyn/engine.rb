08b4af67b44ab42b04563206facceaf07ed354f9
George Opritescu <ssscripting@gmail.com>
2013-09-16 00:12:41 +0300

    more

--- lib/monologue-hathsyn/engine.rb
+++ lib/monologue-hathsyn/engine.rb
@@ -8,6 +8,7 @@ module MonologueHathsyn
     isolate_namespace MonologueHathsyn
 
     def self.activate
+      puts "ACTIVATING"
       Dir.glob(File.join(File.dirname(__FILE__), "../../app/**/*_decorator.rb")) do |c|
         Rails.env.production? ? require(c) : load(c)
       end
