3ce510acaa1bed8a10cf34abd7999f3f21776696
George Opritescu <ssscripting@gmail.com>
2013-09-17 00:05:42 +0300

    Versioned

--- lib/monologue-hathsyn/hathsyn_code_filter.rb
+++ lib/monologue-hathsyn/hathsyn_code_filter.rb
@@ -5,7 +5,10 @@ module MonologueHathsyn
       doc.children.search("[data-lang]").each do |code_node|
         actual_lang = code_node["data-lang"]
         text = code_node.text
-        code_node.replace(CodeRay.scan(text, actual_lang).div)
+        replacement = Rails.cache.fetch([actual_lang, text].join("-")) do
+          CodeRay.scan(text, actual_lang).div
+        end
+        code_node.replace(replacement)
       end
 
       doc
