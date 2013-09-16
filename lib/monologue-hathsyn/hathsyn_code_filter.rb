3bf44cda7540e4c7dffa59316754a6cd6e9e5bf5
George Opritescu <ssscripting@gmail.com>
2013-09-16 20:21:42 +0300

    Try libxml hack(?)

--- lib/monologue-hathsyn/hathsyn_code_filter.rb
+++ lib/monologue-hathsyn/hathsyn_code_filter.rb
@@ -1,7 +1,8 @@
 module MonologueHathsyn
   class HathsynCodeFilter < HTML::Pipeline::Filter
     def call
-      doc.search("[data-lang]").each do |code_node|
+
+      doc.children.search("[data-lang]").each do |code_node|
         actual_lang = code_node["data-lang"]
         text = code_node.text
         code_node.replace(CodeRay.scan(text, actual_lang).div)
